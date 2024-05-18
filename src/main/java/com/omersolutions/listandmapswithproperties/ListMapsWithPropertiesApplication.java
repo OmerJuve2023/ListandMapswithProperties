68f671028cada37178c761f81df81f15826fdff4
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-05-18 16:44:56 -0500

    rename class file to ListandMapswithPropertiesApplication.java for ListMapsWithPropertiesApplication.java

--- src/main/java/com/omersolutions/listandmapswithproperties/ListMapsWithPropertiesApplication.java
+++ src/main/java/com/omersolutions/listandmapswithproperties/ListMapsWithPropertiesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ListandMapswithPropertiesApplication {
+public class ListMapsWithPropertiesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ListandMapswithPropertiesApplication.class, args);
+        SpringApplication.run(ListMapsWithPropertiesApplication.class, args);
     }
 
 }
