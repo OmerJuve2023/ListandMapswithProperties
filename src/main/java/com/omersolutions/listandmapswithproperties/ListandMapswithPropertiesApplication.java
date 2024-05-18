421690e7c6e502aae9b6b22bd5bc456fd0bae8e1
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-05-18 16:42:40 -0500

    Initial commit

---
+++ src/main/java/com/omersolutions/listandmapswithproperties/ListandMapswithPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.omersolutions.listandmapswithproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListandMapswithPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListandMapswithPropertiesApplication.class, args);
+    }
+
+}
